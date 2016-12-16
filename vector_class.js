3cc6058ec01af1fa51cb901e8f1bb2b625902afb
Ayan Majumdar <ayanmajumdar1992@gmail.com>
2016-12-16 13:22:11 +0530

    Create vector_class.js

---
+++ vector_class.js
@@ -0,0 +1,20 @@
+function Vector(x,y)
+{
+  this.x = x;
+  this.y = y;
+}
+
+Vector.prototype = Object.create(null,{
+length: {get: function() {return Math.sqrt(this.x*this.x + this.y*this.y)}}});
+Vector.prototype.plus = function(vector) {
+    return new Vector(this.x+vector.x,this.y+vector.y)};
+Vector.prototype.minus = function(vector) {
+    return new Vector(this.x-vector.x,this.y-vector.y)};
+
+
+console.log(new Vector(1, 2).plus(new Vector(2, 3)));
+// → Vector{x: 3, y: 5}
+console.log(new Vector(1, 2).minus(new Vector(2, 3)));
+// → Vector{x: -1, y: -1}
+console.log(new Vector(3, 4).length);
+// → 5
